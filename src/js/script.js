979bd3e4428771336f88ff4c46c546670d47fbdd
Cecilia Torres <cecilia.torres@htech.com>
2021-12-03 17:47:40 -0800

    dark theme toggle completed

--- src/js/script.js
+++ src/js/script.js
@@ -129,3 +129,18 @@ let selected;
 //level
 
 //role
+
+
+// Dark theme toggle
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
