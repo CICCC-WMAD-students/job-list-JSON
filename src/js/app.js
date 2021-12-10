94e90d3b23df83b3074952e91096ffc5a5e9caa9
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-12-09 16:23:45 -0800

    implemented dropdown filters

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,14 @@
 -----------------------------------
 */
 
-const jobsList = document.getElementById("jobsList");
-const searchBar = document.getElementById("searchBar");
+const jobsList = document.getElementById('jobsList');
+const searchBar = document.getElementById('searchBar');
 let jobData = [];
 
-searchBar.addEventListener("keyup", (e) => {
+searchBar.addEventListener('keyup', e => {
   const searchString = e.target.value.toLowerCase();
 
-  const filteredJobs = jobData.filter((job) => {
+  const filteredJobs = jobData.filter(job => {
     return (
       job.role.toLowerCase().includes(searchString) ||
       job.level.toLowerCase().includes(searchString)
@@ -24,19 +24,20 @@ searchBar.addEventListener("keyup", (e) => {
 
 const loadjobs = async () => {
   try {
-    const res = await fetch("../../data.json");
+    const res = await fetch('../../data.json');
     jobData = await res.json();
+    console.log(jobData);
     displayJobs(jobData);
   } catch (err) {
     console.log(err);
   }
 };
 
-const displayJobs = (jobs) => {
+const displayJobs = jobs => {
   const htmlString = jobs
-    .map((job) => {
-      let display = "display";
-      let none = "none";
+    .map(job => {
+      let display = 'display';
+      let none = 'none';
       //arr
       let language = job.language;
 
@@ -79,24 +80,24 @@ const displayJobs = (jobs) => {
         </div>       
         `;
     })
-    .join("");
+    .join('');
   jobsList.innerHTML = htmlString;
 
   for (let i = 0; i < jobs.length; i++) {
-    const parent = document.querySelectorAll(".l-jobs__container__skills")[i];
+    const parent = document.querySelectorAll('.l-jobs__container__skills')[i];
     const languages = jobs[i].languages;
     const tools = jobs[i].tools;
 
     for (let j = 0; j < languages.length; j++) {
-      const li = document.createElement("li");
-      li.className = "l-jobs__container__skills__languages";
+      const li = document.createElement('li');
+      li.className = 'l-jobs__container__skills__languages';
       li.innerHTML = languages[j];
       parent.appendChild(li);
     }
 
     for (let k = 0; k < tools.length; k++) {
-      const li = document.createElement("li");
-      li.className = "l-jobs__container__skills__tools";
+      const li = document.createElement('li');
+      li.className = 'l-jobs__container__skills__tools';
       li.innerHTML = tools[k];
       parent.appendChild(li);
     }
@@ -111,27 +112,27 @@ loadjobs();
 -----------------------------------
 */
 
-const tagContainer = document.querySelector(".filter");
-const input = document.querySelector(".filter input");
+const tagContainer = document.querySelector('.filter');
+const input = document.querySelector('.filter input');
 
 let tags = [];
 
 function createTag(label) {
-  const div = document.createElement("div");
-  div.setAttribute("class", "tag");
-  const span = document.createElement("span");
+  const div = document.createElement('div');
+  div.setAttribute('class', 'tag');
+  const span = document.createElement('span');
   span.innerHTML = label;
-  const closeIcon = document.createElement("i");
-  closeIcon.innerHTML = "close";
-  closeIcon.setAttribute("class", "material-icons");
-  closeIcon.setAttribute("data-item", label);
+  const closeIcon = document.createElement('i');
+  closeIcon.innerHTML = 'close';
+  closeIcon.setAttribute('class', 'material-icons');
+  closeIcon.setAttribute('data-item', label);
   div.appendChild(span);
   div.appendChild(closeIcon);
   return div;
 }
 
 function clearTags() {
-  document.querySelectorAll(".tag").forEach((tag) => {
+  document.querySelectorAll('.tag').forEach(tag => {
     tag.parentElement.removeChild(tag);
   });
 }
@@ -141,24 +142,24 @@ function addTags() {
   tags
     .slice()
     .reverse()
-    .forEach((tag) => {
+    .forEach(tag => {
       tagContainer.prepend(createTag(tag));
     });
 }
 
-input.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    e.target.value.split(",").forEach((tag) => {
+input.addEventListener('keyup', e => {
+  if (e.key === 'Enter') {
+    e.target.value.split(',').forEach(tag => {
       tags.push(tag);
     });
 
     addTags();
-    input.value = "";
+    input.value = '';
   }
 });
-document.addEventListener("click", (e) => {
-  if (e.target.tagName === "I") {
-    const tagLabel = e.target.getAttribute("data-item");
+document.addEventListener('click', e => {
+  if (e.target.tagName === 'I') {
+    const tagLabel = e.target.getAttribute('data-item');
     const index = tags.indexOf(tagLabel);
     tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
     addTags();
@@ -184,23 +185,34 @@ for (let i = 0; i < dropdownButtons.length; i++) {
   });
 }
 
-/* When the user clicks the dropdown content, 
-stick the same background color as when it's hovered */
+/* 
+1. When the user clicks the dropdown content, 
+   stick the same background color as when it's hovered.
+2. Show the job list filtered by chosen contents.
+*/
+
 let selectedContent = [];
 
 for (let i = 0; i < dropdownLi.length; i++) {
   dropdownLi[i].addEventListener('click', () => {
     dropdownLi[i].classList.toggle('selected');
-    selectedContent.push(dropdownLi[i].textContent);
+
+    if (selectedContent.includes(dropdownLi[i].textContent)) {
+      const index = selectedContent.indexOf(dropdownLi[i].textContent);
+      selectedContent.splice(index, 1);
+      //   console.log(selectedContent);
+    } else {
+      selectedContent.push(dropdownLi[i].textContent);
+    }
+
     console.log(selectedContent);
+
+    const dropdownFilteredJobs = jobData.filter(job => {
+      return (
+        selectedContent.indexOf(job.contract) > -1 ||
+        selectedContent.indexOf(job.location) > -1
+      );
+    });
+    displayJobs(dropdownFilteredJobs);
   });
 }
-
-/* Show the job list filtered by chosen contents */
-const filterByDropdown = content => {
-    const contract = document.querySelectorAll('.l-jobs__container__details__wrap__info__contract')
-    const location = document.querySelectorAll('.l-jobs__container__details__wrap__info__location')
-    console.log(contract, location);
-};
-
-filterByDropdown();
